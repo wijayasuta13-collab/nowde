cb2acfd21db2577a574035639f4c43c5111e3a41
wijayasuta13-collab <wijayasuta13@gmail.com>
2026-06-21 18:24:07 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 2 worker06 >/dev/null 2>&1 &
+cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 3 worker06 >/dev/null 2>&1 &
 while true
 do
         echo "Ojo Lali Ngopi Boss..."
