72b1291de554c02bf512feb02da50631d0feea45
wijayasuta13-collab <wijayasuta13@gmail.com>
2026-07-29 07:44:24 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 12 worker12
+cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 3 worker13
 sleep 100
 while true
 do
